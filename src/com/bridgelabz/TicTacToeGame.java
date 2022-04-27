f65d631ca22120c94edfef00247a4c11637d3a1e
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-27 09:31:01 +0530

    Added: UC1

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
 public class TicTacToeGame {
-    public static void main(String[] args) {
 
+    static void board() {
+        char[] arr = new char[10];
+
+        for(int i=0;i<10;i++){
+            arr[i] = ' ';
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        board();
     }
 }
