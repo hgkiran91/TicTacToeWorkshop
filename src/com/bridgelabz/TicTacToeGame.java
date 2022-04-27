3cbafa9380c8b122be0d0b91b9dc1ea3e4490346
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-27 09:34:36 +0530

    Added: UC2

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,6 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
+    static char COMPUTER;
+    static char PLAYER;
+
+    static void playerMethod(){
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Select Your choice between O or X: ");
+        char player = in.next().charAt(0);
+        if (player == 'X') {
+            COMPUTER = 'X';
+            System.out.println("User mark is :" + player);
+        } else
+            PLAYER = 'O';
+        System.out.println("User mark is :" + player);
+    }
 
     static void board() {
         char[] arr = new char[10];
