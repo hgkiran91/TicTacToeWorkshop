d12f0b2131378c324f742e667767db3c3133fbd8
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-28 09:19:34 +0530

    Added: UC3

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -7,6 +7,21 @@ public class TicTacToeGame {
     static char COMPUTER;
     static char PLAYER;
 
+    static void showBoard(){
+        char[][] board = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-',},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        for (int i =0;i< board.length;i++) {
+            for (int j=0;j< board.length;j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     static void playerMethod(){
         Scanner in = new Scanner(System.in);
         System.out.println(" Select Your choice between O or X: ");
@@ -29,6 +44,7 @@ public class TicTacToeGame {
     }
 
     public static void main(String[] args) {
-        board();
+        //board();
+        showBoard();
     }
 }
